9af9f49e6197bcc2e7fdf4718380f136a76c1186
Sukma Rangga Pradita Wijaya <46914976+sukmaranggapradeta@users.noreply.github.com>
2019-02-12 10:00:39 +0700

    Update Exercises_4_Cari_Modus.js

--- Exercises_4_Cari_Modus.js
+++ Exercises_4_Cari_Modus.js
@@ -1,40 +1,45 @@
 function cariModus(arr) {
   // you can only write your code here!
   var output = []
-  var counter = 1
   for (var x = 0 ; x < arr.length ; x++){
     var flag = false
-    var temp = []
+    var temp = [] //===>reset temp []
+    //
     for (var y = 0 ; y < output.length ; y++){
       if (arr[x] === output[y][0]){
-      output[y][1] += 1
-      //output[y].push(arr[x])
+      output[y][1] += 1 //output ===> [4,2] update
+      //console.log(arr[x] + " === "+output[y][0] )
       flag = true
       }
     }
     if (flag === false){
-      temp.push(arr[x],counter)
-      output.push(temp)
+      //console.log(arr[x] + " masuk ke temp ")
+      temp.push(arr[x],1) //temp ==> [10,1],[4,1],[5,1],[2,1]
+      output.push(temp) //output ===>[[10,1],[4,1],[5,1],[2,1]]
       flag = false
     }
   }
-var maks = 0
-var nilaiMaks = 0
-var modus = 0
-for (var i = 0 ; i< output.length ; i++){
-    //console.log(output [i][0])
-    if (output[i][1]> maks){
-      maks = output[i][1]
-      nilaiMaks = output[i]
+  var maks = 0
+  var nilaiMaks = 0
+  var modus = 0
+  //console.log(output)
+  //cari nilai yang paling sering keluar
+  for (var i = 0 ; i< output.length ; i++){
+      //console.log(output [i][0])
+      if (output[i][1]> maks){
+        maks = output[i][1]
+        nilaiMaks = output[i]
+      }
+    modus += output[i][1]
+    //console.log(output[i][1])
+  } 
+  //console.log(modus)
+    if ((modus % output.length) === 0){
+      //console.log(modus +" ini modus")
+      //console.log(output.length + " ini length")
+      return output = -1
     }
-  modus += output[i][1]
-} 
-  if ((modus % output.length) === 0){
-    //console.log(modus +" ini modus")
-    //console.log(output.length + " ini length")
-    return output = -1
-  }
- // console.log(nilaiMaks[0])
+    //console.log(nilaiMaks[0])
   return nilaiMaks[0]
 }
 
