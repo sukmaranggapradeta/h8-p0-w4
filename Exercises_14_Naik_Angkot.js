83a6f09c2964f656b9dffe988f1351377e91085a
Sukma Rangga Pradita Wijaya <46914976+sukmaranggapradeta@users.noreply.github.com>
2019-02-13 16:24:30 +0700

    tugas w4

---
+++ Exercises_14_Naik_Angkot.js
@@ -0,0 +1,49 @@
+/* 
+Diberikan function naikAngkot(listPenumpang) 
+yang akan menerima satu parameter berupa array dua dimensi. 
+Function akan me-return array of object.
+
+Diberikan sebuah rute, dari A - F. 
+Penumpang diwajibkan membayar Rp2000 setiap melewati satu rute.
+
+Contoh: input: [['Dimitri', 'B', 'F']] 
+output: [{ penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 }]
+ */
+function naikAngkot(arrPenumpang) {
+    rute = ['A', 'B', 'C', 'D', 'E', 'F'];
+    //your code here
+    var newArray = []
+    var naikDari = 0
+    var tujuan = 0
+    if (arrPenumpang.length === 0  || arrPenumpang.length === undefined){
+        return newArray
+    }
+    for (var j = 0 ; j < arrPenumpang.length ; j++){
+        var bayar = 0
+        for (var i = 0 ; i < rute.length ; i++){
+            if (rute[i] === arrPenumpang[j][1]){
+                //console.log("posisi 1 di " + i)
+                naikDari = i
+            }else if (rute[i] === arrPenumpang[j][2]) {
+                //console.log("posisi 2 di " + i)
+                tujuan = i
+            }
+        }
+        bayar = (tujuan - naikDari) * 2000
+
+        var object = {}
+        object.penumpang = arrPenumpang[j][0];
+        object.naikDari = arrPenumpang[j][1];
+        object.tujuan = arrPenumpang[j][2];
+        object.bayar = bayar  
+        newArray.push(object)   
+    }
+    return newArray
+  }
+  
+  //TEST CASE
+  console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]));
+  // [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
+  //   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
+  
+  console.log(naikAngkot([])); //[]
