fd66a14a164c4e6e8d0fd4dec62e60e36844cc5b
Sukma Rangga Pradita Wijaya <46914976+sukmaranggapradeta@users.noreply.github.com>
2019-02-14 16:48:37 +0700

    tugas week 4

---
+++ Exercises_16_Graduates.js
@@ -0,0 +1,98 @@
+function graduates (students) {
+  // Code disini
+    var object = {}
+    //console.log(students.length)
+    for (var i = 0 ; i < students.length ; i++){
+        if (object[students[i].class] === undefined){
+            object[students[i].class] = []
+        }
+      var isiObject = {}
+      for (key in students[i]){
+        if(key !== "class"){
+            isiObject[key] = students[i][key]
+        }
+      }
+      //kondisi jika Student lulus, score lebih besar dari 75.
+      if (students[i].score > 75){
+        object[students[i].class].push(isiObject)
+      }
+    }
+    return object
+}
+
+console.log(graduates([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Dimitri', score: 90 }
+//   ],
+//   wolves: [
+//     { name: 'Alexei' , score: 85 },
+//     { name: 'Anastasia', score: 78 }
+//   ]
+// }
+
+console.log(graduates([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Alexander', score: 100 },
+//     { name: 'Vladimir', score: 92 }
+//   ],
+//   wolves: [
+//     { name: 'Alisa', score: 76 },
+//   ],
+//   tigers: [
+//     { name: 'Viktor', score: 80 }
+//   ]
+// }
+
+
+console.log(graduates([])); //{}
